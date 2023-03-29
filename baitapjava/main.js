a6cd708b104ae825ecb71b884945af993f46e093
tuananh582 <phamtuananh582@gmail.com>
2023-03-29 10:43:29 +0700

    nopthe

--- baitapjava/main.js
+++ baitapjava/main.js
@@ -1,27 +1,6 @@
-// function addContact()
-// {
-//     const name =  document.getElementById("name").value;
-//     const phone = document.getElementById("phone").value;
-//     const contact={
-//         name: name,
-//         phone:phone
-//      };
-     
-//      const list = document.getElementById("mylist");
-//      const li = document.createElement("li");
-//      li.appendChild(document.createTextNode(`${contact.name} -  ${contact.phone}`));
-//      list.appendChild(li);
-//      document.getElementById("name").value=""
-//      document.getElementById("phone").value=""
-// }
 
-
-
-
-//Retrieve the contacts from local storage
 let contacts = JSON.parse(localStorage.getItem("contacts")) || [];
 
-// Display the contacts on the page
 const list = document.getElementById("contacts-list");
 contacts.forEach(function(contact) {
   const li = document.createElement("li");
@@ -30,21 +9,21 @@ contacts.forEach(function(contact) {
 });
 
 function addContact() {
-  // Get the input values from the form
+ 
   const name = document.getElementById("name").value;
   const phone = document.getElementById("phone").value;
 
-  // Create a new contact object
+
   const contact = {
     name: name,
     phone: phone
   };
 
-  // Add the contact to the array and store it in local storage
+
   contacts.push(contact);
   localStorage.setItem("contacts", JSON.stringify(contacts));
 
-  // Add the contact to the list on the page
+  
   const li = document.createElement("li");
   li.textContent = `${contact.name} - ${contact.phone}`;
   list.appendChild(li);
@@ -52,9 +31,24 @@ function addContact() {
   // Clear the form
   document.getElementById("name").value = "";
   document.getElementById("phone").value = "";
-//   alert(name)
-//   alert(phone)
+
 }
 
 
 
+function deletebt(){
+  const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
+  var element = document.getElementById("contacts-list").innerHTML
+  var elmentnum = element.split("")
+  var unit=[]
+  for(let i = 0 ; i < elmentnum.length;i++)
+  {
+    if(!unit.includes(elmentnum[i]))
+    {
+      unit.push(elmentnum[i])
+    }
+  } 
+  element= unit.join("-");  
+
+}
